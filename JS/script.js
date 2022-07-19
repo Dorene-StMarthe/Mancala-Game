97152604ea20a58f8bbe448c4163900f1c851a42
Dorene07 <dstmarth@gmail.com>
2022-07-18 20:34:10 -0400

    tinkering

--- JS/script.js
+++ JS/script.js
@@ -12,23 +12,30 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.display = "block";
     }
-  });
-}
-
+  });}
 
 
 
 //eventlisteners and DOM
-const purplePlayer = document.getElementsByClassName('.hole1');
-const redPlayer = document.getElementsByClassName('.hole2');
-
-document.getElementsByClassName('.gameRows').addEventListener('click', function(){
-    gameStart();
-});
+const playGame = document.querySelector('.gameRows');
+playGame.addEventListener('click', game);
 
 
 
 
+const h0 = document.getElementById("hole0");
+const h1 = document.getElementById("hole1");
+const h2 = document.getElementById("hole2");
+const h3 = document.getElementById("hole3");
+const h4 = document.getElementById("hole4");
+const h5 = document.getElementById("hole5");
+
+const h7 = document.getElementById("hole7");
+const h8 = document.getElementById("hole8");
+const h9 = document.getElementById("hole9");
+const h10 = document.getElementById("hole10");
+const h11 = document.getElementById("hole11");
+const h12 = document.getElementById("hole12");
 
 
 
@@ -36,57 +43,33 @@ document.getElementsByClassName('.gameRows').addEventListener('click', function(
 
 
 
-
-
-
-
-
-//functions
-
-function gameSetup() {
-    gameHoles = [ 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
-    player = "one";
-    winner = null;
+let setUp = function boardSetUp() {
+    h0.textContent = holesArray[hole0];
+    h1.textContent = holesArray[hole1];
+    h2.textContent = holesArray[hole2];
+    h3.textContent = holesArray[hole3];
+    h4.textContent = holesArray[hole4];
+    h5.textContent = holesArray[hole5];
+    h6.textContent = holesArray[hole6];
+    h7.textContent = holesArray[hole7];
+    h8.textContent = holesArray[hole8];
+    h9.textContent = holesArray[hole9];
+    h10.textContent = holesArray[hole10];
+    h11.textContent = holesArray[hole11];
+    h12.textContent= holesArray[hole12];
+    h13.textContent = holesArray[hole13];
 }
-
-
-function gameStart(e){
-    let begin = parseInt(e.target.id.replace('gameHoles', ''));
-    if (player === 'one' && begin > 5) return;
-    if (player === 'two' && (begin <= 6 || move === 13)) return;
-   
-    
-};
-
-marbles= 24;
-marbsPerHole= 4;
-pitsAndHoles= 14;
-
-
-let mancala=[pitsAndHoles];
-let holesPerPlayer = 6;
-let playerMove = pitsAndHoles - 1;
-let initialPlay = 0;
-
-
-
-function gameStart(){
- for ( let move = 0; move < marbsPerHole; move++){
-   if( move == holesPerPlayer || move == playerMove) {
-        mancala[move] = 0;
-   }
-    else {
-        mancala[move] = marbsPerHole;
+let holesArray = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];  
+let marbsInHoles = 4;
+let a = 1;
+function game() {
+    let marbsInHoles = 4;
+    let holesArray = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];  
+    for (let move = 0; move < holesArray.length; move ++){//game starts and moves 13 or less steps
+        if (move == 4 || move <= 13){
+        return marbsInHoles + 1;
     }
-   }
-;}
-
-
-
-
-
-
-
+    else {}}}
 
 
 

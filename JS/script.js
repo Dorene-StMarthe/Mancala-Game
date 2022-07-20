05af3dc80b2a947a44ff00c1e9017e8c3f517f94
Dorene07 <dstmarth@gmail.com>
2022-07-19 22:23:34 -0400

    All mancala pieces on board

--- JS/script.js
+++ JS/script.js
@@ -23,9 +23,12 @@ let startGame = document.querySelector('.startGame')
 startGame.textContent = "Start Game"
 startGame.addEventListener("click", boardDisplay);
 
+let gameRound = document.querySelector('.holes1' ,'.holes2')
+gameRound.addEventListener("click", gameRound);
 
 
-//const playBtn = document.querySelector('.infobar');
+
+
 
 //red
 const h0 = document.getElementById("hole0");
@@ -34,6 +37,7 @@ const h2 = document.getElementById("hole2");
 const h3 = document.getElementById("hole3");
 const h4 = document.getElementById("hole4");
 const h5 = document.getElementById("hole5");
+const h6 = document.querySelector("div.end2");
 //purple
 const h7 = document.getElementById("hole7");
 const h8 = document.getElementById("hole8");
@@ -41,7 +45,7 @@ const h9 = document.getElementById("hole9");
 const h10 = document.getElementById("hole10");
 const h11 = document.getElementById("hole11");
 const h12 = document.getElementById("hole12");
-
+const h13 = document.querySelector("div.end1");
 
 
 let holesArray = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];  
@@ -53,46 +57,34 @@ function boardDisplay() {
     h3.textContent = holesArray[4];
     h4.textContent = holesArray[5];
     h5.textContent = holesArray[6];
-    h6.textContent = holesArray[7];
+    h6.textContent = holesArray[0];
     h7.textContent = holesArray[8];
     h8.textContent = holesArray[9];
     h9.textContent = holesArray[10];
     h10.textContent = holesArray[11];
     h11.textContent = holesArray[12];
     h12.textContent= holesArray[13];
-    
+    h13.textContent = holesArray[7];
 }
 
 
 
+turn =1;
+turn++;
+let idx = holesArray[idx];
+let startingIdx = 
 
-// class Players {
-//     constructor(color, arrayIndex, pitIndex){
-//     this.color = color;
-//     this.arrayIndex= arrayIndex;
-//     this.pitIndex = pitIndex;
-// }}
+function gameRound() {
+    let idx = holesArray[idx];
+        if (holesArray[1,5])
+        for (let i = 0; i = 4 || i === idx; i++){
+            return idx=0; 
 
-// let purplePlayer = new Players(purple, holesArray[1,6], holesArray[0]);
-// let redPlayer = new Players (red, holesArray[8,13], holesArray[7]);
+        }
+}
+console.log(gameRound)
 
 
-// function game(amountIdx) {
-//     let numMarbles = holesArray[amountIdx];
-//     holesArray[amountIdx]=0;
-//     amountIdx += 1;
-//     while (numMarbles >0) {
-//         if (amountIdx > 13 ) amountIdx = 0;
-//         if (amountIdx !==13 || amountIdx !== 6){
-//             holesArray[amountIdx]++;
-//             numMarbles--;
-//         }
-//         amountIdx++;
-//     }
-//     return --amountIdx;
-    
-// }
-// //console.log(game(3));
 
 
 
